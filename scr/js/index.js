87de836aea8baeafadff9449d1d07cc6756b21d3
agustinpardolp <agustinpardo@hotmail.com>
2020-01-18 15:12:40 -0300

    maquetado de html y css resposivo final, se corrigieron menu, imagen de banner, tamaño de logo, se agrego pagina de terminos y condiciones

---
+++ scr/js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $(".hamburger-nav").on("click", function(){
+
+      // $(".menu").fadeToggle("slow").toggleClass("menu-hide");
+      $(".menu").animate({
+        height: 'toggle'
+      });
+
+  });
+
+});
+
+$(document).ready(function(){
+
+  $(".categorias-nav").on("click", function(){
+
+      // $(".menu").fadeToggle("slow").toggleClass("menu-hide");
+      $(".menu").animate({
+        height: 'toggle'
+      });
+
+  });
+
+});
+
